0beded3aa6bfedbb8a3124918414a0bde003d9aa
Ryan Beckett <mr.r.beckett@gmail.com>
2020-04-29 14:21:48 +0100

    Improved preference panel

--- Focusin/Preferences/PreferencesWindowController.swift
+++ Focusin/Preferences/PreferencesWindowController.swift
@@ -19,7 +19,7 @@ class PreferencesWindowController: NSWindowController, NSWindowDelegate {
     var delegate: PreferencesDelegate?
     
     let defaults = UserDefaults.standard
-
+    
     @IBOutlet weak var pomodoroDuration: NSTextField!
     @IBOutlet weak var shortBreakDuration: NSTextField!
     @IBOutlet weak var longBreakAfterXPomodoros: NSTextField!
@@ -65,46 +65,6 @@ class PreferencesWindowController: NSWindowController, NSWindowDelegate {
         return NSNib.Name(rawValue: "PreferencesWindowController")
     }
     
-//    override var windowNibName : String! {
-//        return "PreferencesWindowController"
-//    }
-//
-    /* Save the current settings and close the window */
-    @IBAction func savePreferences(_ sender: AnyObject) {
-        closedWithButton = true
-        
-        if(pomodoroDuration.integerValue < MIN_TIME || pomodoroDuration.integerValue > MAX_TIME) {
-            dialogError(errorPomodoro)
-        } else if(shortBreakDuration.integerValue < MIN_TIME || shortBreakDuration.integerValue > MAX_TIME) {
-            dialogError(errorBreak)
-        } else if(longBreadDuration.integerValue < MIN_TIME || longBreadDuration.integerValue > MAX_TIME) {
-            dialogError(errorBreak)
-        } else if(targetPomodoros.integerValue < MIN_TARGET || targetPomodoros.integerValue > MAX_TARGET) {
-            dialogError(errorTarget)
-        } else if(longBreakAfterXPomodoros.integerValue < MIN_TARGET || longBreakAfterXPomodoros.integerValue > MAX_TARGET) {
-            dialogError(errorLongBreak)
-        } else {
-            defaults.setValue(pomodoroDuration.integerValue * seconds, forKey: Defaults.pomodoroKey)
-            defaults.setValue(shortBreakDuration.integerValue * seconds, forKey: Defaults.shortBreakKey)
-            defaults.setValue(longBreadDuration.integerValue * seconds, forKey: Defaults.longBreakKey)
-            defaults.setValue(longBreakAfterXPomodoros.integerValue, forKey: Defaults.longBreakAfterXPomodoros)
-            defaults.setValue(targetPomodoros.integerValue, forKey: Defaults.targetKey)
-            defaults.setValue(showNotifications.state, forKey: Defaults.showNotificationsKey)
-            defaults.setValue(showTimeInBar.state, forKey: Defaults.showTimeKey)
-            defaults.setValue(startAtLogin.state, forKey: Defaults.startAtLogin)
-
-            let launcherAppIdentifier = "com.albertoquesada.LauncherApplication"
-            SMLoginItemSetEnabled(launcherAppIdentifier as CFString, startAtLogin.state == NSControl.StateValue.on)
-            
-            closeAndSave()
-            self.window?.close()
-        }
-    }
-    
-    /* Notify of changes to delegates */
-    func closeAndSave() {
-        delegate?.preferencesDidUpdate()
-    }
     
     /* Show an alert to the user */
     func dialogError(_ text: String) -> Bool {
@@ -119,5 +79,45 @@ class PreferencesWindowController: NSWindowController, NSWindowDelegate {
         }
         return false
     }
-
+    
+    func windowShouldClose(_ sender: NSWindow) -> Bool {
+        if !isValidSettings(){
+            self.dialogError(errorBreak)
+            return false
+        }
+        saveSettings()
+        delegate?.preferencesDidUpdate()
+        return true
+    }
+    
+    func isValidSettings() -> Bool{
+        if(pomodoroDuration.integerValue < MIN_TIME || pomodoroDuration.integerValue > MAX_TIME) {
+            return false
+        } else if(shortBreakDuration.integerValue < MIN_TIME || shortBreakDuration.integerValue > MAX_TIME) {
+            return false
+        } else if(longBreadDuration.integerValue < MIN_TIME || longBreadDuration.integerValue > MAX_TIME) {
+            return false
+        } else if(targetPomodoros.integerValue < MIN_TARGET || targetPomodoros.integerValue > MAX_TARGET) {
+            return false
+        } else if(longBreakAfterXPomodoros.integerValue < MIN_TARGET || longBreakAfterXPomodoros.integerValue > MAX_TARGET) {
+            return false
+        }
+        return true
+    }
+    
+    func saveSettings() {
+        defaults.setValue(pomodoroDuration.integerValue * seconds, forKey: Defaults.pomodoroKey)
+        defaults.setValue(shortBreakDuration.integerValue * seconds, forKey: Defaults.shortBreakKey)
+        defaults.setValue(longBreadDuration.integerValue * seconds, forKey: Defaults.longBreakKey)
+        defaults.setValue(longBreakAfterXPomodoros.integerValue, forKey: Defaults.longBreakAfterXPomodoros)
+        defaults.setValue(targetPomodoros.integerValue, forKey: Defaults.targetKey)
+        defaults.setValue(showNotifications.state, forKey: Defaults.showNotificationsKey)
+        defaults.setValue(showTimeInBar.state, forKey: Defaults.showTimeKey)
+        defaults.setValue(startAtLogin.state, forKey: Defaults.startAtLogin)
+        
+        let launcherAppIdentifier = "com.albertoquesada.LauncherApplication"
+        SMLoginItemSetEnabled(launcherAppIdentifier as CFString, startAtLogin.state == NSControl.StateValue.on)
+    }
+    
+    
 }
